05fee95cb4f60a01f6f99309d19c1f85fb459228
Daniel Hjort <daniel.hjort@patchworksolutions.com>
2011-03-15 17:46:06 +0800

    Initial Commit

---
+++ TracksObjCTests/TracksObjCTests.h
@@ -0,0 +1,17 @@
+//
+//  TracksObjCTests.h
+//  TracksObjCTests
+//
+//  Created by Daniel Hjort on 3/15/11.
+//  Copyright 2011 Patchwork Solutions AB. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TracksObjCTests : SenTestCase {
+@private
+    
+}
+
+@end
